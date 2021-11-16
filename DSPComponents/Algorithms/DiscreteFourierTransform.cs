13ed419dab49fccc429cc7e959eb7056454450ca
aymanred121 <ayman20191700160@cis.asu.edu.eg>
2021-11-16 04:39:08 +0200

    Add support to DFT and IDFT algorithm

--- DSPComponents/Algorithms/DiscreteFourierTransform.cs
+++ DSPComponents/Algorithms/DiscreteFourierTransform.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,96 @@ namespace DSPAlgorithms.Algorithms
         public Signal InputTimeDomainSignal { get; set; }
         public float InputSamplingFrequency { get; set; }
         public Signal OutputFreqDomainSignal { get; set; }
+        public List<float> outputAmps;
+        public List<float> outputFreq;
+        public List<float> FrequenciesPhaseShifts;
+        public List<Complex> sigmoidList;
+        public float nofSample;
+        public class Complex
+        {
+            public Complex()
+            {
+                Real = 0;
+                Imaginary = 0;
+            }
+            public Complex(float real, float imaginary)
+            {
+                Real = real;
+                Imaginary = imaginary;
+            }
+
+            public float Real { get; set; }
+           public float Imaginary { get; set; }
+       
+        }
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            Initalizer();
+            DFT();
+            SaveToFile();
+
+
+         
+        }
+
+        private void SaveToFile()
+        {
+            string fileName = @"C:\Users\ayman\Desktop\5th semster\DSP\DFT.txt";
+            if (!File.Exists(fileName))
+            {
+
+                using (StreamWriter writer = File.CreateText(fileName))
+                {
+                   // writer.WriteLine(outputAmps.Count+" "+FrequenciesPhaseShifts.Count);
+                    for(int i = 0; i < nofSample; i++)
+                    {
+                        writer.WriteLine(OutputFreqDomainSignal.FrequenciesAmplitudes[i] + " " + OutputFreqDomainSignal.FrequenciesPhaseShifts[i]);
+                    }
+                }
+            }
+        }
+
+        private void Initalizer()
+        {
+            outputAmps = new List<float>();
+            outputFreq = new List<float>();
+            FrequenciesPhaseShifts = new List<float>();
+            sigmoidList = new List<Complex>();
+            nofSample = InputTimeDomainSignal.Samples.Count;
+        }
+
+        private void DFT()
+        {
+            float nofSample = InputTimeDomainSignal.Samples.Count;
+            Complex c = new Complex();
+            for (float k = 0; k < nofSample; k++)
+            {
+                c.Real = 0;
+                c.Imaginary = 0;
+                outputAmps.Add(k);
+                for (int n = 0; n < nofSample; n++)
+                {
+                    var expPower = (2 * k * n) / nofSample;
+                    c.Real += (float)(Math.Cos(expPower * Math.PI)) * InputTimeDomainSignal.Samples[n];
+                    c.Imaginary += (float)(-1 * Math.Sin(expPower * Math.PI)) * InputTimeDomainSignal.Samples[n];
+                }
+                outputFreq.Add((float)Math.Sqrt(c.Real * c.Real + c.Imaginary * c.Imaginary));
+                FrequenciesPhaseShifts.Add(CalFreqPhas(c));
+                sigmoidList.Add(c);
+            }
+            OutputFreqDomainSignal = new Signal(false, outputAmps, outputFreq, FrequenciesPhaseShifts);
+        }   
+        private float CalFreqPhas(Complex c)
+        {
+
+            if (c.Real > 0)
+               return((float)Math.Atan(c.Imaginary / c.Real));
+            else if (c.Real < 0 && c.Imaginary >= 0)
+                return((float)(Math.Atan(c.Imaginary / c.Real) + Math.PI));
+            else if (c.Real < 0 && c.Imaginary < 0)
+               return((float)(Math.Atan(c.Imaginary / c.Real) - Math.PI));
+            return 0;
         }
     }
 }
--- DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
+++ DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
@@ -12,9 +12,81 @@ namespace DSPAlgorithms.Algorithms
         public Signal InputFreqDomainSignal { get; set; }
         public Signal OutputTimeDomainSignal { get; set; }
 
+        public List<float> FrequenciesPhaseShifts;
+        public List<float> outputFreq;
+        public List<Complex> expValues;
+        public class Complex
+        {
+            public Complex()
+            {
+                Real = 0;
+                Imaginary = 0;
+            }
+            public Complex(float real, float imaginary)
+            {
+                Real = real;
+                Imaginary = imaginary;
+            }
+
+            public float Real { get; set; }
+            public float Imaginary { get; set; }
+
+        }
+
+
+
         public override void Run()
         {
-            throw new NotImplementedException();
+            Initializer();
+            ADFT();
+
         }
+
+        private void Initializer()
+        {
+            FrequenciesPhaseShifts = new List<float>();
+            outputFreq = new List<float>();
+            expValues = new List<Complex>();
+        }
+
+        private void ADFT()
+        {
+            float nofSample = InputFreqDomainSignal.FrequenciesAmplitudes.Count;
+            CalculateComplexList();
+            for (int k = 0; k < nofSample; k++)
+            {
+                double RealxReal=0;
+                double RealxImg=0;
+                double ImgxImg=0;
+                double ImgxReal=0;
+                for(int n = 0; n < nofSample; n++)
+                {
+                    var exPower = (2 * n * k * Math.PI) / nofSample;
+                    RealxReal += expValues[n].Real * Math.Cos(exPower);
+                    RealxImg += expValues[n].Real * Math.Sin(exPower);
+                    ImgxImg += (-1) * expValues[n].Imaginary * Math.Sin(exPower);
+                    ImgxReal += expValues[n].Imaginary * Math.Cos(exPower);
+
+                }
+                Console.WriteLine(RealxReal + RealxImg + ImgxImg + ImgxReal);
+                outputFreq.Add((float)(RealxReal+RealxImg+ImgxImg+ImgxReal)/nofSample);
+            }
+              OutputTimeDomainSignal = new Signal(outputFreq, false);
+        }
+
+        private void CalculateComplexList()
+        {
+            float nofSample = InputFreqDomainSignal.FrequenciesAmplitudes.Count;
+   
+            for (int k = 0; k < nofSample; k++)
+            {
+               
+                var x = InputFreqDomainSignal.FrequenciesAmplitudes[k] * (float)Math.Cos(InputFreqDomainSignal.FrequenciesPhaseShifts[k]);
+                var y = InputFreqDomainSignal.FrequenciesAmplitudes[k] * (float)Math.Sin(InputFreqDomainSignal.FrequenciesPhaseShifts[k]);
+                expValues.Add(new Complex(x,y));
+            }
+        }
+
+      
     }
 }
